f1b88482a27224433081050e02bba55df558f919
Lucat0n <39059724+Lucat0n@users.noreply.github.com>
2020-04-05 18:22:16 +0200

    google java formatted

--- src/main/java/Hello_world.java
+++ src/main/java/Hello_world.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Hello_world {
-    public static void main(String[] arg) {
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.println("Please, give me your name");
-        if (inputScanner.hasNext()) {
-            String input = inputScanner.nextLine();
-            System.out.println("Hello, " + input);
-        } else {
-            System.out.println("Incorrect input");
-        }
+  public static void main(String[] arg) {
+    Scanner inputScanner = new Scanner(System.in);
+    System.out.println("Please, give me your name");
+    if (inputScanner.hasNext()) {
+      String input = inputScanner.nextLine();
+      System.out.println("Hello, " + input);
+    } else {
+      System.out.println("Incorrect input");
     }
+  }
 }
