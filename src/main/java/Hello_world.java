913b93989a24f313770d2164635d4d6fc0ea1e52
Lucat0n <39059724+Lucat0n@users.noreply.github.com>
2020-04-05 18:53:20 +0200

    add Javadoc comment

--- src/main/java/Hello_world.java
+++ src/main/java/Hello_world.java
@@ -1,8 +1,27 @@
 import java.util.Scanner;
 
+/***
+ * Klasa, przy pomocy której jest pobierane imię użytkownika, a następnie wyświetlana wiadomość powitalna z jego imieniem.
+ */
 public class Hello_world {
+
+  /***
+   *  Scanner umożliwiający pobranie danych z wejścia.
+   */
+  static Scanner inputScanner = new Scanner(System.in);
+
+  /***
+   * Główna metoda w programie.
+   * @param arg Argumenty przekazywane z linii poleceń
+   */
   public static void main(String[] arg) {
-    Scanner inputScanner = new Scanner(System.in);
+    printHello();
+  }
+
+  /***
+   * Metoda pobierająca i wyświetlająca imię użytkownika.
+   */
+  private static void printHello(){
     System.out.println("Please, give me your name");
     if (inputScanner.hasNext()) {
       String input = inputScanner.nextLine();
